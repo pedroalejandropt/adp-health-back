994d9a978cd7ba37a8cf717ec60830830ea49057
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-03-01 02:37:49 -0400

    UPDATE: Email as unique variable

--- models/user.model.js
+++ models/user.model.js
@@ -13,7 +13,8 @@ module.exports = (sequelize, Sequelize) => {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       password: {
         type: Sequelize.STRING
